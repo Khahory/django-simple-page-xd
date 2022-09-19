5d845240c10d4800ab17cb1d80ada258ea90f9f8
Khahory <angeld05@live.com>
2022-09-19 08:56:24 -0400

    renderiza un view de un template

--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -11,7 +11,11 @@ def index(request):
     return HttpResponse('que si cameto que si')
 
 
-# noinspection PyUnresolvedReferences
 def book_by_id(request, book_id):
     book = Book.objects.get(pk=book_id)
     return HttpResponse(f"Book: {book.title}, publicado el {book.pub_date}")
+
+
+def book_details_render(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return render(request, 'book_details.html', {'book': book})
