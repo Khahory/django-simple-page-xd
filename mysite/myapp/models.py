c7ce86096224137f4eef25044086ec35cc0cbfda
Khahory <angeld05@live.com>
2022-09-19 08:51:48 -0400

    get book from db

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
 
+# para hacer la migracion mira los comandos aqui:
+# https://www.youtube.com/watch?v=ZsJRXS_vrw0
+
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(max_length=200)
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -4,7 +4,14 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Book
 
 
 def index(request):
     return HttpResponse('que si cameto que si')
+
+
+# noinspection PyUnresolvedReferences
+def book_by_id(request, book_id):
+    book = Book.objects.get(pk=book_id)
+    return HttpResponse(f"Book: {book.title}, publicado el {book.pub_date}")
